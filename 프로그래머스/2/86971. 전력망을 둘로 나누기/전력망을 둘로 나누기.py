a1428573f22834cbc434bffb781eb67a3d753f2a
junh9541 <74410135+junh9541@users.noreply.github.com>
2024-02-19 10:44:12 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 510.44 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -0,0 +1,24 @@
+def solution(n, wires):
+    answer = n
+    for i in range(len(wires)):
+        answer=min(checkWires(wires[:i]+wires[i+1:], n), answer)
+    return answer
+
+def checkWires(wires, n):
+    checked=0
+    nodesNum=len(set(sum(wires, [])))
+    nodes=set([])
+    if len(wires)==1:
+        return 1
+    for i in range(len(wires)):
+        for j , wire in enumerate(wires):
+            newNodes = nodes.union(wire)
+            # print(newNodes)
+            if len(newNodes)==2 or len(newNodes)-len(nodes)==1:
+                nodes=newNodes
+    
+    print(wires, nodes, abs(n-len(nodes)*2))
+    return abs(n-len(nodes)* 2)
+    
+    
+    
