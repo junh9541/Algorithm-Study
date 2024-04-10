f4141f608e023e4ef5a28ec6b3674c8a4db5b203
junh9541 <74410135+junh9541@users.noreply.github.com>
2024-04-10 13:39:25 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,23 @@
+from heapq import heapify, heappop, heappush
+import math
+def solution(jobs):
+    startTimeHeap=[tuple(v) for v in jobs[:]]
+    heapify(startTimeHeap)
+    workTimeHeap=[]
+    
+    time=0
+    workTimeSum=0
+    
+    while(len(startTimeHeap) or len(workTimeHeap)):
+        if len(startTimeHeap):
+            if len(workTimeHeap)==0 or startTimeHeap[0][0]<=time:
+                nearByWork = heappop(startTimeHeap)
+                heappush(workTimeHeap, (nearByWork[1], nearByWork[0]))
+                continue
+        currWork=heappop(workTimeHeap)
+        workTimeSum+=(currWork[0]+max(time, currWork[1])-currWork[1])
+        time=max(time, currWork[1])+currWork[0]
+        
+    return math.floor(workTimeSum/len(jobs))
+
+        
